91320b8c05061998009d500c2f26c26ed976cea2
yusluv1234 <yusluv124@gmail.com>
2021-08-31 22:58:24 +0100

    please take note of this

--- src/components/about/About.Component.jsx
+++ src/components/about/About.Component.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-// import AboutDark from '../icons/images/image-about-dark.jpg';
-// import AboutLight from '../icons/images/image-about-light.jpg';
+// import React from 'react';
+// // import AboutDark from '../icons/images/image-about-dark.jpg';
+// // import AboutLight from '../icons/images/image-about-light.jpg';
 
-import './About.Styles.scss';
+// import './About.Styles.scss';
 
-const AboutComponent = () => {
-    return (
-        <div className='about'>
-            <div className='first-image'>
-            {/* <img className='photo' src={AboutDark} alt=''  /> */}
-            </div>
-            <div className='about-comment'>
-                <div className='comment__header'>
-                    ABOUT OUR FURNITURE 
-                </div>
-                    <p>Our multifunctional collection blengs design and function to suit your
-                    indivikdual taste. Make each room unique, or pick a cohesive theme that
-                    best express your interests and what inspires you. Find the furniture pieces
-                    you need, from traditional to contemporary styles or anything in between. Product
-                    specialist are available to help you create your dream space.
-                </p>
-            </div>
-            <div className='second-image'>
-            {/* <img className='photo' src={AboutLight} alt='' /> */}
+// const AboutComponent = () => {
+//     return (
+//         <div className='about'>
+//             <div className='first-image'>
+//             {/* <img className='photo' src={AboutDark} alt=''  /> */}
+//             </div>
+//             <div className='about-comment'>
+//                 <div className='comment__header'>
+//                     ABOUT OUR FURNITURE
+//                 </div>
+//                     <p>Our multifunctional collection blengs design and function to suit your
+//                     indivikdual taste. Make each room unique, or pick a cohesive theme that
+//                     best express your interests and what inspires you. Find the furniture pieces
+//                     you need, from traditional to contemporary styles or anything in between. Product
+//                     specialist are available to help you create your dream space.
+//                 </p>
+//             </div>
+//             <div className='second-image'>
+//             {/* <img className='photo' src={AboutLight} alt='' /> */}
 
-            </div>
-            
-        </div>
-    )
-}
+//             </div>
 
-export default AboutComponent;
+//         </div>
+//     )
+// }
+
+// export default AboutComponent;
